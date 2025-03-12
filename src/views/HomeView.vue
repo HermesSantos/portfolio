99c5b91deb5ad387998688e152bedc7e4720e0d8
HermesSantos <hermessantos995@gmail.com>
2025-03-12 11:47:18 -0300

    Implement mouse hover effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,35 @@
-<script setup>
+<script setup lang="ts">
 import Header from '../components/Header.vue'
 import About from '../components/About.vue'
+import {ref} from 'vue'
+
+const position = ref({
+  x: 0,
+  y: 0
+})
+function onMouseMove (e: any) {
+  position.value.x = e.clientX
+  position.value.y = e.clientY
+}
 </script>
 
 <template>
-  <main>
+  <main @mousemove="onMouseMove">
     <Header />
     <About />
+    <div class="mouse-effect"
+      :style="{top: `${position.y}px`, left: `${position.x}px`}">
+    </div>
   </main>
 </template>
+<style scoped>
+.mouse-effect {
+  height: 5rem;
+  pointer-events: none;
+  width: 5rem;
+  background: rgb(0,85,230);
+  filter: blur(4rem);
+  background: linear-gradient(90deg, rgba(0,85,230,1) 0%, rgba(126,21,191,1) 31%, rgba(19,96,182,1) 52%, rgba(97,18,174,1) 74%, rgba(9,9,121,1) 100%, rgba(0,212,255,1) 100%);
+  position: absolute;
+}
+</style>
