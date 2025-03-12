beb840c20451de0cd578fc52b9bc995f09195edc
HermesSantos <hermessantos995@gmail.com>
2025-03-12 10:40:36 -0300

    Feat Add title attributes to skill icons and enhance hover effect

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,32 +7,32 @@
       <div class="grid grid-cols-3 lg:grid-cols-6 gap-4 md:gap-8">
         <div class="animate-slide-in delay-100">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="JavaScript">
               <img alt="JavaScript icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/javascript-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 JavaScript</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="TypeScript">
               <img alt="TypeScript icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/typescript-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 TypeScript</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Laravel">
               <img alt="CSS icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/laravel-logo-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Laravel</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="VueJS">
               <img alt="CSS icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/vue-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Vue</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="HTML">
               <img alt="HTML icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/html-5-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 HTML</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="CSS">
               <img alt="CSS icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/css-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 CSS</span>
@@ -41,27 +41,27 @@
         </div>
         <div class="animate-slide-in delay-200">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="React">
               <img alt="React icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/react-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 React</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Next.Js">
               <img alt="Next.js icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/nextjs-icon-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Next.js</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Tailwind">
               <img alt="Tailwind icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/tailwind-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Tailwind</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="NPM">
               <img alt="NPM icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/npm-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 NPM</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Deno">
               <img alt="Deno icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/Deno_Logo_2024.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Deno</span>
@@ -70,32 +70,32 @@
         </div>
         <div class="animate-slide-in delay-300">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Node.js">
               <img alt="Node.js icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/node-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Node.js</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Express">
               <img alt="Express icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/express-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Express</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="PostgreSQL">
               <img alt="PostgreSQL icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/postgresql-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 PostgreSQL</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Firebase">
               <img alt="Firebase icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/firebase-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Firebase</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="PHP">
               <img alt="PHP icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/php-1-logo-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 PHP</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="SQL">
               <img alt="SQL icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/sql-database-generic-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 SQL</span>
@@ -104,32 +104,32 @@
         </div>
         <div class="animate-slide-in delay-400">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Docker">
               <img alt="Docker icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/docker-svgrepo-com(1).svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Docker</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Git">
               <img alt="Git icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/git-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Git</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Github">
               <img alt="GitHub icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/github-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 GitHub</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Gitlab">
               <img alt="GitLab icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/gitlab-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 GitLab</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="AWS">
               <img alt="AWS icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/aws-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 AWS</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Nginx">
               <img alt="Nginx icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/nginx-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Nginx</span>
@@ -138,27 +138,27 @@
         </div>
         <div class="animate-slide-in delay-500">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Linux">
               <img alt="Linux icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/linux-svgrepo-com(1).svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Linux</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Ubuntu">
               <img alt="Ubuntu icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/ubuntu-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Ubuntu</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Arch">
               <img alt="Arch icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/arch-linux-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Arch</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Slack">
               <img alt="Slack icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/slack-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Slack</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Vim/Neovim">
               <img alt="Vim icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/vim-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Vim</span>
@@ -167,32 +167,32 @@
         </div>
         <div class="animate-slide-in delay-600">
           <div class="space-y-2 md:space-y-4">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="VSCode">
               <img alt="VS Code icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/vscode-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
-                VS Code</span>
+                VSCode</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Terminal">
               <img alt="Terminal icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/terminal-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Terminal</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="PowerShell">
               <img alt="PowerShell icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/powershell-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 PowerShell</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Bash">
               <img alt="Bash icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/bash-icon-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Bash</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Notion">
               <img alt="Notion icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/notion-logo-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Notion</span>
             </div>
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2" title="Figma">
               <img alt="Figma icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" class="w-5 h-5" style="color:transparent" src="../../public/icons/icons/figma-svgrepo-com.svg">
               <span class="text-xs md:text-sm font-medium text-foreground">
                 Figma</span>
@@ -206,5 +206,13 @@
 <style scoped lang="scss">
 .contact-buttons-main{
   margin-top: 5rem;
+}
+img {
+  transition: width .3s;
+}
+img:hover{
+  width: 1.5rem;
+  height: 1.5rem;
+  transition: .3s ease-in-out;
 }
 </style>
