6d52b068c2e10dc4e81a2962a08b260008f8c9bc
HermesSantos <hermessantos995@gmail.com>
2025-03-12 10:59:36 -0300

    Refactor: Update base URL and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: '/hermes-dev',
+  base: '/portfolio',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
