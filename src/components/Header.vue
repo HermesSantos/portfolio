85735b87cd54c0e3fd2a17d90fdda5670af3bb83
HermesSantos <hermessantos995@gmail.com>
2025-03-12 09:27:02 -0300

    feat Implement contact modal in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import {ref} from 'vue'
+import ModalContact from './ContactModal.vue';
+
+const show = ref(false);
+const handleModal = () => {
+  show.value = !show.value
+}
 </script>
 
 <template>
   <div class="header-main">
+    <ModalContact :show="show" @close-modal="handleModal"/>
     <div class="header-items">
       <a href="#about-section" class="header-link-item">Sobre</a>
       <a href="#tech-section" class="header-link-item">Tecnologias</a>
       <!-- <a href="#projects-section" class="header-link-item">Projetos</a> -->
       <!-- <a href="#xp-section" class="header-link-item">Experiências</a> -->
-      <a href="#contact-section" class="header-link-item">Contatos</a>
+      <button @click="handleModal" class="header-link-item">Contatos</button>
     </div>
   </div>
 </template>
