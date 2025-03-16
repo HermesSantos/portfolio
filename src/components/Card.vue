9cd1f97419a9afa41946845bd379f69cc011d09f
HermesSantos <hermessantos995@gmail.com>
2025-03-16 14:07:19 -0300

    feat Display projects dynamically from GitHub API

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,46 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  projectName: String,
+  projectDescription: String,
+  projectLanguage: String,
+  projectUrl: String
+})
+
+</script>
 <template>
   <div class="max-w-sm border-2 border-solid text-white rounded overflow-hidden shadow-lg">
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-      <p class="text-white text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </p>
+      <div class="font-bold text-xl mb-2">{{projectName}}</div>
+      <p class="text-white text-base">{{projectDescription}}</p>
     </div>
     <div class="px-6 pt-4 pb-2">
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      <div>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{{projectLanguage}}</span>
+      </div>
+      <div class="mt-2 flex align-center justify-center px-2 pt-2 pb-2">
+        <button target="_blank" :href="projectUrl" data-modal-target="default-modal" data-modal-toggle="default-modal" @click="handleModalContacs">Saber Mais</button>
+      </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
+p {
+  text-transform: capitalize;
+}
+button {
+  margin: 0 1rem;
+  padding: .5rem 2rem;
+  background-color: #7953cd;
+  border-radius: .3rem;
+  background: none;
+  border: 2px solid grey;
+  font-size: .8rem;
+  cursor: pointer;
+}
+button:hover {
+  border: 2px solid #7953cd;
+  transition: 0.3s;
+}
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
 import Card from "./Card.vue"
+import {onMounted, ref} from 'vue'
+
+onMounted(() => {
+  fetch('https://api.github.com/users/HermesSantos/repos', {
+    method: 'get'
+  }).then(response => response.json())
+    .then(data => {
+      projects.value = data.filter(el => el.name === 'aila' || el.name === 'auto-like')
+      console.log(projects.value)
+    })
+})
+
+const projects = ref()
+
 </script>
 <template>
-  <div class="flex flex-col items-center justify-center px-20 pt-35">
+  <div class="flex flex-col items-center justify-center px-10 pt-15">
     <h1 class="text-5xl pb-10">Projetos</h1>
   </div>
-  <Card />
+  <div class="flex flex-row align-center justify-center px-10 pt-10 pb-10 gap-4">
+    <Card v-for="project in projects"
+      :key="project.id"
+      :projectName="project.name"
+      :projectDescription="project.description"
+      :projectLanguage="project.language"
+      :projectUrl="project.clone_url"
+    />
+  </div>
 </template>
 <style lang="scss" scoped>
 </style>
